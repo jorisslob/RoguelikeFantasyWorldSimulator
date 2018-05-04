5e4aa544612ac1b2598ec268547456cc5713a511
Joris Slob <joriss@topdesk.com>
2018-05-04 19:10:14 +0200

    Add buffer for graphics

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
 
 tdl.set_font('arial10x10.png', greyscale=True, altLayout=True)
-console = tdl.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="Roguelike Fantasy World Simulator", fullscreen=False)
+root = tdl.init(SCREEN_WIDTH, SCREEN_HEIGHT,
+                title="Roguelike Fantasy World Simulator", fullscreen=False)
+con = tdl.init(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 tdl.setFPS(LIMIT_FPS)
 
 playerx = SCREEN_WIDTH//2
 playery = SCREEN_HEIGHT//2
 
+
 def handle_keys():
     global playerx, playery
 
-    #realtime
+    # realtime
     keypress = False
     for event in tdl.event.get():
         if event.type == 'KEYDOWN':
@@ -24,21 +27,21 @@ def handle_keys():
 
     if not keypress:
         return
-    
+
     if user_input.key == 'ENTER' and user_input.alt:
-        #Alt+Enter: toggle fullscreen
+        # Alt+Enter: toggle fullscreen
         tdl.set_fullscreen(not tdl.get_fullscreen())
 
     elif user_input.key == 'ESCAPE':
-        return True #exit game
-    
-    #movement keys
+        return True  # exit game
+
+    # movement keys
     if user_input.key == 'UP':
         playery -= 1
-    
+
     elif user_input.key == 'DOWN':
         playery += 1
-    
+
     elif user_input.key == 'LEFT':
         playerx -= 1
 
@@ -46,13 +49,13 @@ def handle_keys():
         playerx += 1
 
 
-
 while not tdl.event.is_window_closed():
-    console.draw_char(playerx, playery, '@', bg=None, fg=(255,255,255))
+    con.draw_char(playerx, playery, '@', bg=None, fg=(255, 255, 255))
+    root.blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0)
     tdl.flush()
 
-    #handle keys and exit game if needed
-    console.draw_char(playerx, playery, ' ', bg=None)
+    # handle keys and exit game if needed
+    con.draw_char(playerx, playery, ' ', bg=None)
     exit_game = handle_keys()
     if exit_game:
         break
