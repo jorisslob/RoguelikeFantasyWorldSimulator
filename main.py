87f28ef7948d4517e6594ba1aacca9e666ccfd3e
Joris Slob <joriss@topdesk.com>
2018-05-04 18:48:50 +0200

    Show the @ on screen

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import tdl
+
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+LIMIT_FPS = 20
+
+tdl.set_font('arial10x10.png', greyscale=True, altLayout=True)
+console = tdl.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="Roguelike Fantasy World Simulator", fullscreen=False)
+
+tdl.setFPS(LIMIT_FPS)
+
+while not tdl.event.is_window_closed():
+    console.draw_char(1, 1, '@', bg=None, fg=(255,255,255))
+    tdl.flush()
