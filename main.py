c5e5d302347848ff71b6f95e80699ab4175a9bbc
Joris Slob <joriss@topdesk.com>
2018-05-09 16:00:46 +0200

    Pull Rect out of main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tile import Tile
+from rect import Rect
 
 from random import randint
 
@@ -29,25 +30,6 @@ color_dark_wall = (0, 0, 100)
 color_light_wall = (130, 110, 50)
 color_dark_ground = (50, 50, 150)
 color_light_ground = (200, 180, 50)
-
-
-class Rect:
-    # A rectangle on the map, used to characterize a room
-    def __init__(self, x, y, w, h):
-        self.x1 = x
-        self.y1 = y
-        self.x2 = x + w
-        self.y2 = y + h
-
-    def center(self):
-        center_x = (self.x1 + self.x2) // 2
-        center_y = (self.y1 + self.y2) // 2
-        return (center_x, center_y)
-
-    def intersect(self, other):
-        # returns true if this rectangle intersects with another one
-        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
-                self.y1 <= other.y2 and self.y2 >= other.y1)
 
 
 class GameObject:
